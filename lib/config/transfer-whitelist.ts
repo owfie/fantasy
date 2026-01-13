12d9cbd905e9afb61ef85ac250ce86b513f0a332
alfie <0xalfie@gmail.com>
2026-01-13 10:41:19 +1030

    debug: add uuids

--- lib/config/transfer-whitelist.ts
+++ lib/config/transfer-whitelist.ts
@@ -3,6 +3,7 @@
 export const TRANSFER_WINDOW_BYPASS_WHITELIST: string[] = [
   // Example: '2eb0941a-b6bf-418a-a711-4db9426f5161',
   '63b4efa2-3597-4942-8bc9-958c39b5785e',
+  '2eb0941a-b6bf-418a-a711-4db9426f5161'
 ];
 
 export function canBypassTransferWindow(userId: string | undefined): boolean {
